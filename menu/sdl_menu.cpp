ca99dbe031a19a43f18d3750ab628bfedf4eb69e
KyomaHooin <richard.bruna@protonmail.com>
2019-03-02 14:14:01 +0100

    More menu code.

--- menu/sdl_menu.cpp
+++ menu/sdl_menu.cpp
@@ -4,58 +4,49 @@
 
 const int MENU_SCREEN_WIDTH = 576;
 const int MENU_SCREEN_HEIGHT = 532;
-const int MENU_TEXT_OFFSET = 20;
-
-//const int GAME_SLOT_WIDTH = 286;
-//const int GAME_SLOT_HEIGHT = 130;
-const int GAME_SLOT_OFFSET = 3;
-const int GAME_SLOT_IMG_OFFSET = 3;
-const int GAME_SLOT_IMG_SIZE = 174;
+const int GAME_SLOT = 200;
+const int GAME_SLOT_IMG= 174;
 
 const char *gamefn[4] = {"flappy.bmp","boulder.bmp","manic.bmp","fred.bmp"};
 const char *gametext[4] = {"Flappy","Boulder Dash","Manic Miner","Fred"};
 
-void DrawGameRect(SDL_Renderer *renderer, SDL_Rect border) {
-
+void RenderGameMenu(SDL_Renderer *renderer, SDL_Rect border) {
 	for (int i = 0; i < 4; i++) {
 		char imgpath[40] = "/usr/local/share/gpmd85emu/";
 		strcat(imgpath,gamefn[i]);
-
-		//	printf("Image: %s \n", imgpath);
-		SDL_Rect slot_rect, img_rect;
+		//printf("Image: %s \n", imgpath);
+		SDL_Rect slot_border, img_border;
 		SDL_Surface *gameimg = SDL_LoadBMP(imgpath);
 		SDL_Texture *imgtexture = SDL_CreateTextureFromSurface(renderer,gameimg);
 		SDL_FreeSurface(gameimg);
+		
+		slot_border.w = GAME_SLOT;
+		slot_border.h = GAME_SLOT;
+		img_border.w = GAME_SLOT_IMG;
+		img_border.h = GAME_SLOT_IMG;
 
-		slot_rect.w = (MENU_SCREEN_WIDTH - 4*GAME_SLOT_OFFSET) / 2;
-		slot_rect.h = (MENU_SCREEN_HEIGHT - 4*GAME_SLOT_OFFSET) / 2;
-		img_rect.w = GAME_SLOT_IMG_SIZE;
-		img_rect.h = GAME_SLOT_IMG_SIZE;
+		int slot_offset_x = (border.w - 2*GAME_SLOT) / 3;
+		int slot_offset_y = (border.h - 2*GAME_SLOT) / 3;
 
 		switch(i) {
 			case 0:
-				slot_rect.x = border.w + GAME_SLOT_OFFSET;
-				slot_rect.y = border.h + GAME_SLOT_OFFSET;
-				img_rect.x = slot_rect.x + GAME_SLOT_IMG_OFFSET;
-				img_rect.y = slot_rect.y + (slot_rect.h - img_rect.w) / 2;
+				slot_border.x = border.x + slot_offset_x;
+				slot_border.y = border.y + slot_offset_y;
 			case 1:
-				slot_rect.x = border.w + slot_rect.w + 3*GAME_SLOT_OFFSET;
-				slot_rect.y = border.h + GAME_SLOT_OFFSET;
-				img_rect.x = slot_rect.x + GAME_SLOT_IMG_OFFSET;
-				img_rect.y = slot_rect.y + (slot_rect.h - img_rect.w) / 2;
+				slot_border.x = border.x + 2*slot_offset_x + GAME_SLOT;
+				slot_border.y = border.y + slot_offset_y;
 			case 2:
-				slot_rect.x = border.w + GAME_SLOT_OFFSET;
-				slot_rect.y = border.h + slot_rect.h + 3*GAME_SLOT_OFFSET;
-				img_rect.x = slot_rect.x + GAME_SLOT_IMG_OFFSET;
-				img_rect.y = slot_rect.y + (slot_rect.h - img_rect.w) / 2;
+				slot_border.x = border.x + slot_offset_x;
+				slot_border.y = border.y + 2*slot_offset_y + GAME_SLOT;
 			case 3:
-				slot_rect.x = border.w + slot_rect.w + 3*GAME_SLOT_OFFSET;
-				slot_rect.y = border.h + slot_rect.h + 3*GAME_SLOT_OFFSET;
-				img_rect.x = slot_rect.x + GAME_SLOT_IMG_OFFSET;
-				img_rect.y = slot_rect.y + (slot_rect.h - img_rect.w) / 2;
+				slot_border.x = border.x + 2*slot_offset_x + GAME_SLOT;
+				slot_border.y = border.y + 2*slot_offset_y + GAME_SLOT;
 		}
-		SDL_RenderDrawRect(renderer,&slot_rect);// slot border
-		SDL_RenderCopy(renderer,imgtexture, NULL, &img_rect); // game img texture
+		img_border.x = slot_border.x + (GAME_SLOT - GAME_SLOT_IMG)/2;
+		img_border.y = slot_border.y + (GAME_SLOT - GAME_SLOT_IMG)/2;
+		
+		SDL_RenderDrawRect(renderer,&slot_border);
+		SDL_RenderCopy(renderer,imgtexture, NULL, &img_border);
 	}
 	return;
 }
@@ -101,11 +92,11 @@ int main(int argc, char* args[]) {
 			SDL_RenderClear(renderer);
 			SDL_SetRenderDrawColor(renderer,255,255,255,255);// white
 		
-			SDL_RenderDrawRect(renderer,&border);// menu border
-			DrawGameRect(renderer,border); // slot border + game BMP image
+			SDL_RenderDrawRect(renderer,&border);
+			RenderGameMenu(renderer,border);
 
 			SDL_RenderPresent(renderer);
-			SDL_Delay(5000);
+			SDL_Delay(10000);
 			SDL_DestroyRenderer(renderer);
 		}
 		SDL_DestroyWindow(window);
