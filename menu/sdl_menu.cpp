69029b470548f8b7abd2768c7f427eb2dcd9814d
KyomaHooin <richard.bruna@protonmail.com>
2019-03-08 13:50:19 +0100

    Menu text + font, fix redraw logic.

--- menu/sdl_menu.cpp
+++ menu/sdl_menu.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <stdio.h>
 #include <bcm_host.h>
 
@@ -17,6 +18,7 @@ const int MENU_BORDER_X = (1360 - MENU_WIDTH) / 2;
 const int MENU_BORDER_Y = (768 - MENU_HEIGHT) / 2 ;
 const int MENU_IMAGE = 174;
 const int MENU_IMAGE_OFFSET = (MENU_BORDER_WIDTH - MENU_IMAGE) / 2;
+const int MENU_FONT_SIZE = 24;
 const int grey[4] = {0, 25, 100, 255};// dark to white
 const char *gamefn[4] {
 		"/usr/local/share/gpmd85emu/flappy.bmp",
@@ -24,78 +26,85 @@ const char *gamefn[4] {
 		"/usr/local/share/gpmd85emu/manic.bmp",
 		"/usr/local/share/gpmd85emu/fred.bmp"
 	};
+const char *gametext[4] {"Flappy", "Boulder Dash", "Manic Miner", "Fred"};
 
 //--- FUNC ---
 
-void Highlight(SDL_Renderer *renderer, int game, int color) {
-	SDL_Rect border_rect;
+void RenderText(SDL_Renderer *renderer) {
+	TTF_Font *font;
+	SDL_Surface* text_surface;
+	SDL_Texture* text;
+	SDL_Rect text_rect;
+	SDL_Color text_color = {255,255,255};
 
-	border_rect.w = MENU_BORDER_WIDTH;
-	border_rect.h = MENU_BORDER_HEIGHT;
-
-	SDL_SetRenderDrawColor(renderer, grey[color], grey[color], grey[color], 255);
-
-	border_rect.x = MENU_BORDER_X + MENU_SPACING + game * (MENU_BORDER_WIDTH + MENU_SPACING);
-	border_rect.y = MENU_BORDER_Y + MENU_SPACING;
-
-	SDL_RenderDrawRect(renderer,&border_rect);
-	SDL_RenderPresent(renderer);
+	font = TTF_OpenFont("atari.ttf", MENU_FONT_SIZE);
+	text_rect.h = MENU_FONT_SIZE;
+	for (int i=0; i < 4; i++) {
+		text_surface = TTF_RenderText_Solid(font, gametext[i], text_color);
+		text = SDL_CreateTextureFromSurface(renderer, text_surface);
+		text_rect.w = 16 * strlen(gametext[i]);
+		text_rect.x = MENU_BORDER_X + MENU_SPACING + (MENU_BORDER_WIDTH -text_rect.w) / 2 + i * (MENU_BORDER_WIDTH + MENU_SPACING);
+		text_rect.y = MENU_BORDER_Y + MENU_SPACING + MENU_IMAGE_OFFSET + MENU_IMAGE + 29;
+		SDL_RenderCopy(renderer, text, NULL, &text_rect);
+	}
+	SDL_FreeSurface(text_surface);
+	SDL_DestroyTexture(text);
 }
 
-void RenderMenuDefault(SDL_Renderer *renderer) {
-	SDL_Surface* bmp = NULL;
-	SDL_Texture* image = NULL;
-	SDL_Rect img_rect,border_rect;
-
+void RenderImage(SDL_Renderer *renderer) {
+	SDL_Surface* bmp;
+	SDL_Texture* image;
+	SDL_Rect img_rect;
 	img_rect.w = MENU_IMAGE;
 	img_rect.h = MENU_IMAGE;
-	border_rect.w = MENU_BORDER_WIDTH;
-	border_rect.h = MENU_BORDER_HEIGHT;
-
-	SDL_SetRenderDrawColor(renderer, 25, 25, 25, 255);// border color
-
-	image = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, MENU_IMAGE, MENU_IMAGE);
-	if(image == NULL) {
-		printf("Image texture init error.\n");
-	}
-
 	for (int i=0; i < 4; i++) {
 		bmp = SDL_LoadBMP(gamefn[i]);
 		image = SDL_CreateTextureFromSurface(renderer, bmp);
 		img_rect.x = MENU_BORDER_X + MENU_SPACING + MENU_IMAGE_OFFSET + i * (MENU_BORDER_WIDTH + MENU_SPACING);
 		img_rect.y = MENU_BORDER_Y + MENU_SPACING + MENU_IMAGE_OFFSET;
-
 		SDL_RenderCopy(renderer, image, NULL, &img_rect);
-
-		border_rect.x = MENU_BORDER_X + MENU_SPACING + i * (MENU_BORDER_WIDTH + MENU_SPACING);
-		border_rect.y = MENU_BORDER_Y + MENU_SPACING;
-
-		SDL_RenderDrawRect(renderer,&border_rect);
 	}
-	SDL_RenderPresent(renderer);
 	SDL_FreeSurface(bmp);
 	SDL_DestroyTexture(image);
+}
+
+void RenderMenu(SDL_Renderer *renderer, int game_next) {
+	SDL_Rect border_rect;
+	border_rect.w = MENU_BORDER_WIDTH;
+	border_rect.h = MENU_BORDER_HEIGHT;
+
+	RenderImage(renderer);// Render images
+	RenderText(renderer);// Render texts
+
+	for (int i=0; i < 4; i++) {
+		if ( i != game_next ) { SDL_SetRenderDrawColor(renderer, 25, 25, 25, 255);
+	       	} else { SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); };
+		border_rect.x = MENU_BORDER_X + MENU_SPACING + i * (MENU_BORDER_WIDTH + MENU_SPACING);
+		border_rect.y = MENU_BORDER_Y + MENU_SPACING;
+		SDL_RenderDrawRect(renderer, &border_rect);
+	}
+	SDL_RenderPresent(renderer);
 }
 
 //--- MAIN ---
 
 int main(int argc, char* args[]) {
 	bool quit = false;
-	int game = 0, color = 0;
+	int game_next = 0;
 	unsigned int nextTime;
 
 	SDL_Window* window = NULL;
 	SDL_Renderer* renderer = NULL;
 	SDL_Event event;
-
+	TTF_Font *font = NULL;
 	//SDL_RendererInfo driver;
 	//SDL_DisplayMode fullscreen;
 
 	bcm_host_init();// ?
 
 	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_EVENTS) != 0) return 1;
+	if(TTF_Init() != 0 ) printf("Font init failed.\n");
 	//if(SDL_GetRenderDriverInfo(0,&driver) != 0) printf("Failed to get driver info.");
-
 	//printf("Current driver: %s\n",driver.name);
 	//printf("      Software: %c\n",(driver.flags & SDL_RENDERER_SOFTWARE) ? 'x': ' ');
 	//printf("   Accelerated: %c\n",(driver.flags & SDL_RENDERER_ACCELERATED) ? 'x': ' ');
@@ -118,37 +127,31 @@ int main(int argc, char* args[]) {
 	SDL_SetRenderDrawColor(renderer,0,0,0,255);
 	SDL_RenderClear(renderer);// clrscr	
 
-	RenderMenuDefault(renderer);//menu default
+	RenderMenu(renderer,game_next);
 
 	while(1) {
-		nextTime = SDL_GetTicks() + 500;//highlight delay 500ms
+		nextTime = SDL_GetTicks() + 100;
 		while(SDL_PollEvent(&event)) {
 			switch(event.type) {
 				case SDL_KEYDOWN:
 					if (event.key.keysym.sym == SDLK_LEFT) {
-						(game != 0) ? game-- : game = 3;
-						printf("Left pressed. Game: %i ", game);
+						(game_next == 0) ? game_next = 3 : game_next--;
+						RenderMenu(renderer,game_next);
 					}
 					if (event.key.keysym.sym == SDLK_RIGHT) {
-						(game == 3) ? game = 0 : game++;
-						printf("Right pressed. Game: %i ", game);
+						(game_next == 3) ? game_next = 0 : game_next++;
+						RenderMenu(renderer, game_next);
 					}
 					if (event.key.keysym.sym == SDLK_RETURN) {
 						quit = true;
-						printf("Enter Game: %i\n", game);
+						printf("Enter Game: %i\n", game_next);
 				       	}
 					break;
 				default:
 					break;
 			}
 		}
-
 		if (quit) { break; }
-
-		printf("Highlighting, game: %i color: %i\n", game, color);
-		Highlight(renderer, game, color);
-		(color == 3) ? color = 0 : color++;
-	
 		while (SDL_GetTicks() < nextTime) SDL_Delay(1);//prevent CPU exhaustion
 	}
 	SDL_DestroyRenderer(renderer);
