fa1b851891296b85afe0e3eec6f6cfc81da6c257
KyomaHooin <richard.bruna@protonmail.com>
2019-09-15 18:27:43 +0200

    Menu rewrite.

--- simpmd/menu/menu.cpp
+++ simpmd/menu/menu.cpp
@@ -24,13 +24,13 @@ const char *gameFile[4] {
   "/root/simpmd-develop/menu/logo/fred.bmp"
 };
 
-const char *gametext[4] {"FLAPPY", "BOULDER", "MANIC", "FRED"};
+const char *gameText[4] {"FLAPPY", "BOULDER", "MANIC", "FRED"};
 
 // Menu
 bool emulatorQuit = false;
 bool inMenu = true;
 int gameIndex = 0;
-//unsigned int nextTime;
+unsigned int nextTime;
 
 // Main window
 SDL_Window* mainWindow;
@@ -51,7 +51,6 @@ void RenderText(SDL_Renderer *renderer) {
   SDL_Rect textRectangle;
   SDL_Color textColor = {255,255,255};// white
 
-  //textFont = TTF_OpenFont("arcade.ttf", 24);
   textFont = TTF_OpenFont("atari.ttf", 24);
 
   for (int i = 0; i < 4; i++) {
@@ -75,7 +74,7 @@ void RenderImage(SDL_Renderer *renderer) {
   SDL_Texture* imageTexture;
   SDL_Rect imageRectangle;
 
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < 4; i++) {
     imageSurface = SDL_LoadBMP(gameFile[i]);
     imageTexture = SDL_CreateTextureFromSurface(renderer, imageSurface);
  
@@ -86,27 +85,28 @@ void RenderImage(SDL_Renderer *renderer) {
   
     SDL_RenderCopy(renderer, imageTexture, NULL, &imageRectangle);
   }
-
   SDL_FreeSurface(imageSurface);
   SDL_DestroyTexture(imageTexture);
 }
 
 // Render complete menu and game select frame
 void RenderMenu(SDL_Renderer *renderer, int gameIndex) {
-	SDL_Rect frameRectangle;
+  SDL_Rect frameRectangle;
 
-	frameRectangle.w = 200;
-	frameRectangle.h = 260;
-	frameRectangle.x = 100 - 13 + gameIndex * 200;// 0-px h-spacing
-	frameRectangle.y = 300 - 13;
-	// Prepare static content
-	RenderText(renderer);
-	RenderImage(renderer);
-	// Selected game restangle
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);// white frame
-	SDL_RenderDrawRect(renderer, &frameRectangle);
-	//Present All the content
-	SDL_RenderPresent(renderer);
+  for (int i = 0; i < 4; i++) {
+    frameRectangle.w = 200;
+    frameRectangle.h = 260;
+    frameRectangle.x = 100 - 13 + gameIndex * 200;// 0-px h-spacing
+    frameRectangle.y = 300 - 13;
+    if (i == gameIndex) {
+      SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);// white frame
+    } else {
+      SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);// black frame
+    }
+    SDL_RenderDrawRect(renderer, &frameRectangle);
+  }
+  // Present complete menu
+  SDL_RenderPresent(renderer);
 }
 
 // Run game
@@ -116,23 +116,17 @@ void RenderMenu(SDL_Renderer *renderer, int gameIndex) {
 //	//Emulator exit
 //}
 
-// exit menu
-void menuQuit() {
-	SDL_DestroyRenderer(menuRenderer);
-}
-
 //Menu timer callback function
-Uint32 menuTimerCallback (Uint32 menuInterval, void *pArgs)
-{
-  SDL_Event menuEvent;
-  menuEvent.type = SDL_USEREVENT;
-  menuEvent.user.code = 1;
-  menuEvent.user.data1 = NULL;
-  menuEvent.user.data2 = NULL;
-  SDL_PushEvent (&menuEvent);
-
-  return menuInterval;
-}
+//Uint32 menuTimerCallback (Uint32 menuInterval, void *pArgs) {
+//  SDL_Event menuEvent;
+//  menuEvent.type = SDL_USEREVENT;
+//  menuEvent.user.code = 1;
+//  menuEvent.user.data1 = NULL;
+//  menuEvent.user.data2 = NULL;
+//  SDL_PushEvent (&menuEvent);
+//
+//  return menuInterval;
+//}
 
 //
 //------------------------------------------------------ MAIN ---
@@ -141,14 +135,18 @@ Uint32 menuTimerCallback (Uint32 menuInterval, void *pArgs)
 int main(int argc, char* args[]) {
 
   if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_EVENTS) != 0) return 1;
+  printf("SDL: done..\n");
 
   if(TTF_Init() != 0 ) printf("Font init failed.\n");
+  printf("TTF: done..\n");
 	
   mainWindow = SDL_CreateWindow("PMD-85", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 200, 0);
   if(mainWindow == NULL) printf("Window init error.\n");
+  printf("Window: done..\n");
 
   menuRenderer = SDL_CreateRenderer(mainWindow, 0, SDL_RENDERER_ACCELERATED);//SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE
   if(menuRenderer == NULL) printf("Renderer init error.\n");
+  printf("Renderer: done..\n");
   //Disable cursor
   SDL_ShowCursor(SDL_DISABLE);
   //CLRSCR
@@ -156,12 +154,16 @@ int main(int argc, char* args[]) {
   SDL_RenderClear(menuRenderer);	
   SDL_RenderPresent(menuRenderer);
  
+  // Prepare static content
+  RenderText(menuRenderer);
+  RenderImage(menuRenderer);
+  //Render game frame..
   //RenderMenu(menuRenderer,0);
-  menuTimer = SDL_AddTimer(40, menuTimerCallback, NULL);// 40ms  menu refresh timer ..
+  //menuTimer = SDL_AddTimer(40, menuTimerCallback, NULL);// 40ms  menu refresh timer ..
 
   while(!emulatorQuit) {
 		
-    //nextTime = SDL_GetTicks() + 100;
+    nextTime = SDL_GetTicks() + 100;
 	
     SDL_PollEvent(&sEvent);
     switch(sEvent.type) {
@@ -171,27 +173,34 @@ int main(int argc, char* args[]) {
           if (sEvent.key.keysym.sym == SDLK_LEFT) { (gameIndex == 0) ? gameIndex = 3 : gameIndex--; }
           if (sEvent.key.keysym.sym == SDLK_RIGHT) { (gameIndex == 3) ? gameIndex = 0 : gameIndex++; }
           if (sEvent.key.keysym.sym == SDLK_RETURN) {
-            printf("Enter Game: %i\n", gameIndex);// Emulator()
+            printf("Running emualor %i\n", gameIndex);
 	    inMenu = false;
 	  }
 	}
-        if (sEvent.key.keysym.sym == SDLK_ESCAPE) { emulatorQuit = true; };
-        break;
-      case SDL_USEREVENT:
-	if (sEvent.user.code == 1) {
-          RenderMenu(menuRenderer, gameIndex);
+        if (sEvent.key.keysym.sym == SDLK_ESCAPE) {
+          if(inMenu) {
+            emulatorQuit = true;
+	  } else {
+            printf("Returing to menu..\n");
+	    inMenu = true;
+	  }
         }
         break;
+//      case SDL_USEREVENT:
+//        if (sEvent.user.code == 1) {// menu rendering..
+//          RenderMenu(menuRenderer, gameIndex);
+//        }
+//      break;
     }
 
-    //while (SDL_GetTicks() < nextTime) SDL_Delay(1);//prevent CPU exhaustion
+    //Re-render menu
+    RenderMenu(menuRenderer,gameIndex);
+    while (SDL_GetTicks() < nextTime) SDL_Delay(1);//prevent CPU exhaustion
   }
 
-  //menu cleanup
-  SDL_RemoveTimer(menuTimer);
-  menuQuit();
+  //SDL_RemoveTimer(menuTimer);
 
-  //Quit
+  //QUIT
   SDL_DestroyRenderer(menuRenderer);
   SDL_DestroyWindow(mainWindow);
   SDL_Quit();
